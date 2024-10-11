f07ce5bda6b16501f95bf0ae484584073de73fc5
mythilipriya <mythilipriya.e@diggibyte.com>
2024-10-11 21:06:27 +0530

    converting

---
+++ pyspark/converting.py
@@ -0,0 +1,36 @@
+from pyspark.shell import spark
+from pyspark.sql import SparkSession
+
+spark= SparkSession.builder \
+         .appName("name") \
+         .getOrCreate()
+sc=spark.sparkContext
+data=[(1, "Alice"), (2, "Bob"), (3, "Charlie"),(1,"Alice")]
+rdd=sc.parallelize(data)
+
+#RDD to DataFrame:
+df=rdd.toDF(["id","name"])
+# df.show()
+#print(type(df))
+#-------------------------------------------------------------------------
+
+#Dataframe to RDD:
+
+# rdd2=df.rdd
+# print(rdd.collect())
+# print(type(rdd2))
+#---------------------
+# df.select("name","id").show()  ---SELECT
+#df.filter(df.id > 1).show()     ---FILTER
+from pyspark.sql.functions import col, aggregate
+
+#nc=df.withColumn("age",col("id")+20).show() ----Add newcolumn
+#rn=df.withColumnRenamed("name","fullname").show() ---rename
+
+#d=df.drop("id").show() ---delete column
+#agg=df.groupBy("name").count().show()  ---aggregate()
+#s=df.sort(df.name.desc()).show()   ---sorting
+
+#l=df.limit(1).show()    ---limit
+#d=df.distinct().show()    ---remove duplicates
+#s=df.orderBy(df.name.desc()).show() --orderby
