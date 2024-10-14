84784041ea6ada5ac0d0bb67098cd5d3737ffa52
mythilipriya <mythilipriya.e@diggibyte.com>
2024-10-14 21:02:43 +0530

    define schema with Array

---
+++ pyspark/arraytype.py
@@ -0,0 +1,32 @@
+from numpy.f2py.symbolic import integer_types
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+spark= SparkSession.builder \
+         .appName("name") \
+         .getOrCreate()
+sc=spark.sparkContext
+
+#ARRAY:
+
+# data=[('Sam',['Web'],()),('Ram',["DE","DS"]),('mythili',["De","maths"])]
+# schema=StructType([
+# StructField("name",StringType()),
+# StructField("skill",StringType())
+# ])
+
+#Nested Array:
+
+data=[('Sam',['Web'],(1,73026896)),('Ram',["DE","DS"],(2,98737609)),('mythili',["De","maths"],(2,2979787))]
+schema=StructType([
+StructField("name",StringType()),
+StructField("skill",StringType()),
+StructField("details",IntegerType())
+])
+df = spark.createDataFrame(data,schema)
+df.show()
+
+
+
+
+
+
