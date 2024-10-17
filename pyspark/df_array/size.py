2c0bd1278eac7ad6b911087785457f028f97803b
mythilipriya <mythilipriya.e@diggibyte.com>
2024-10-17 20:18:32 +0530

    size of array

---
+++ pyspark/df_array/size.py
@@ -0,0 +1,12 @@
+from pyspark.python.pyspark.shell import spark
+from pyspark.sql.functions import *
+
+data = [
+    (1, ["apple", "banana", "orange"]),
+    (2, ["grape", "mango", "apple"]),
+    (3, ["strawberry", "kiwi"])
+]
+
+df = spark.createDataFrame(data, ["id", "fruits"])
+df1=df.withColumn('result',size('fruits')).show()
+
